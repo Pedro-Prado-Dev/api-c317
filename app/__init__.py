346a17788f44c92a2ee3dcecb21379cf51bcfdc3
Pedro-Prado-Dev <pedroppaiva1@hotmail.com>
2024-10-08 18:57:47 -0300

    :sparkles: feat:api to c317 project

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from app.config import Config
+from app.extensions import db, migrate, api
+from app.users.routes import UserResource
+from app.group.routes import GroupUserResource, GroupResource, GroupListResource
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    api.init_app(app)
+
+    with app.app_context():
+        print("Registered routes:")
+        print(app.url_map)
+
+    return app
