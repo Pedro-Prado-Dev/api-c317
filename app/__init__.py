4c640413f8ead2021c4e663ec918c5afa6c95e01
Pedro-Prado-Dev <pedroppaiva1@hotmail.com>
2024-10-29 19:21:55 -0300

    :sparkles: feat: new form model

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from app.config import Config
 from app.extensions import db, migrate, api
 from app.users.routes import UserResource
 from app.group.routes import GroupUserResource, GroupResource, GroupListResource
+from app.form.routes import FormComponentResource, FormResource, ComponentOptionResource
 
 
 def create_app(config_class=Config):
